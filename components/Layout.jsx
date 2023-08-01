2ce9302b81c550711a86b7d5e370d968053c4656
Santosh Jain <48131320+jain-santosh@users.noreply.github.com>
2023-08-01 21:22:39 +0530

    Update Layout.jsx
    Title Change

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className='layout'>
       <Head>
-        <title>Goat Store</title>
+        <title>SoundSonic Store</title>
       </Head>
       <header>
         <Navbar />
